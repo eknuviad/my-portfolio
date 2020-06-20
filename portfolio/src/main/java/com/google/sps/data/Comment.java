14ea49b4ed7b1a56d75927cd37928099d61f2de8
eknuviad <edemnuviadenu@gmail.com>
2020-06-20 18:18:21 -0400

    Add support for image uploads with Blobstorage

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -5,12 +5,14 @@ public final class Comment {
   private final long id;
   private final String name;
   private final String message;
+  private final String imageUrl;
   private final long timestamp;
 
-  public Comment(long id, String name, String message, long timestamp) {
+  public Comment(long id, String name, String message, String imageUrl, long timestamp) {
     this.id = id;
     this.name = name;
     this.message = message;
+    this.imageUrl = imageUrl;
     this.timestamp = timestamp;
   }
 }
